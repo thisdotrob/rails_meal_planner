2a939b9764a8abc83228aa3cc3567a87a56e6d1a
Rob Stevenson <robert.stevenson@shopify.com>
2021-12-16 16:48:05 +0000

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: Rails.application.credentials.basic_auth_name!, password: Rails.application.credentials.basic_auth_password!
 end
