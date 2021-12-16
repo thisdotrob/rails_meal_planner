f071a205702b6af0b508f6512f87713a978239e5
Rob Stevenson <robert.stevenson@shopify.com>
2021-12-16 16:13:33 +0000

    Add Ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,19 @@
 class IngredientsController < ApplicationController
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.create(ingredient_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.find(params[:id])
+    @ingredient.destroy
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+    def ingredient_params
+      params.require(:ingredient).permit(:quantity_per_serving, :food_item_name, :food_item_unit)
+    end
 end
