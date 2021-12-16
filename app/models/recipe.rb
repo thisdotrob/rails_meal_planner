7beff560d7b86aac54ac70be25272fd8db9795f1
Rob Stevenson <robert.stevenson@shopify.com>
2021-12-16 16:36:37 +0000

    Add Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :ingredients, dependent: :destroy
+
+  validates :title, presence: true
+  validates :instructions, presence: true, length: { minimum: 10 }
 end
+
