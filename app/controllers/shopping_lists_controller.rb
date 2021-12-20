e23152f4d43fcea3026b28a41d8382f5cce02444
Rob Stevenson <robert.stevenson@shopify.com>
2021-12-20 14:02:51 +0000

    Update ShoppingLists controller for ShoppingListItems

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -3,8 +3,35 @@ class ShoppingListsController < ApplicationController
     @meal_plan = MealPlan.find(params[:meal_plan_id])
     if @meal_plan.shopping_list.nil?
       @meal_plan.create_shopping_list
+      shopping_list_items_data = []
+      @meal_plan.meals.each do |meal|
+        meal.recipe.ingredients.each do |ingredient|
+          quantity = ingredient.quantity_per_serving * meal.servings
+          item_data = shopping_list_items_data.find { |item|
+            item[:food_item_id] == ingredient.food_item_id
+          }
+          if !item_data
+            shopping_list_items_data << {
+              food_item_id: ingredient.food_item_id,
+              food_item_name: ingredient.food_item_name,
+              food_item_unit: ingredient.food_item_unit,
+              quantity: quantity,
+            }
+          else
+            item_data[:quantity] += quantity
+          end
+        end
+      end
+      @meal_plan.shopping_list.shopping_list_items.create(shopping_list_items_data)
     end
 
     @shopping_list = @meal_plan.shopping_list
+  end
+
+  def destroy
+    @meal_plan = MealPlan.find(params[:meal_plan_id])
+    @meal_plan.shopping_list.destroy
+
+    redirect_to root_path
   end
 end
