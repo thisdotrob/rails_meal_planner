038a5c3164fe3fe33b8bcacb53bee484dc8934b0
Rob Stevenson <robert.stevenson@shopify.com>
2022-01-07 23:01:45 +0000

    Remove singular MealPlanDate note editing

--- app/controllers/meal_plan_days_controller.rb
+++ app/controllers/meal_plan_days_controller.rb
@@ -1,18 +1,4 @@
 class MealPlanDaysController < ApplicationController
-  def edit
-    @meal_plan_day = MealPlanDay.find(params[:id])
-  end
-
-  def update
-    @meal_plan_day = MealPlanDay.find(params[:id])
-
-    if @meal_plan_day.update(meal_plan_day_params)
-      redirect_to @meal_plan_day.meal_plan
-    else
-      render :edit
-    end
-  end
-
   def copy
     template_mpd = MealPlanDay.find(params[:id])
     template_mpd.meal_plan.meal_plan_days.each { |mpd|
