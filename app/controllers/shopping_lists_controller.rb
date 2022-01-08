a33505ab6e6e6529c66766ec811bf8e02da8f6fb
Rob Stevenson <robert.stevenson@shopify.com>
2022-01-08 14:41:39 +0000

    Fix ShoppingList creation

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -4,21 +4,21 @@ class ShoppingListsController < ApplicationController
     if @meal_plan.shopping_list.nil?
       @meal_plan.create_shopping_list
       shopping_list_items_data = []
-      @meal_plan.meals.each do |meal|
-        meal.recipe.ingredients.each do |ingredient|
-          quantity = ingredient.quantity_per_serving * meal.servings
-          item_data = shopping_list_items_data.find { |item|
-            item[:food_item_id] == ingredient.food_item_id
-          }
-          if !item_data
-            shopping_list_items_data << {
-              food_item_id: ingredient.food_item.id,
-              food_item_name: ingredient.food_item.name,
-              food_item_unit: ingredient.food_item.unit,
-              quantity: quantity,
+      @meal_plan.meal_plan_days.each do |meal_plan_day|
+        meal_plan_day.meals.each do |meal|
+          meal.recipe.ingredients.each do |ingredient|
+            quantity = ingredient.quantity_per_serving * meal.servings
+            item_data = shopping_list_items_data.find { |item|
+              item[:food_item_id] == ingredient.food_item_id
             }
-          else
-            item_data[:quantity] += quantity
+            if !item_data
+              shopping_list_items_data << {
+                food_item_id: ingredient.food_item.id,
+                quantity: quantity,
+              }
+            else
+              item_data[:quantity] += quantity
+            end
           end
         end
       end
