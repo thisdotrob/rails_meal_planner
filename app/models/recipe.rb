d6b1bcf3763b7a66c74b02a2cb5f805b0df8b04f
Rob Stevenson <robert.stevenson@shopify.com>
2022-01-10 12:22:25 +0000

    Allow Recipes without instructions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,5 @@ class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
 
   validates :title, presence: true
-  validates :instructions, presence: true, length: { minimum: 10 }
 end
 
