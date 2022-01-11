2d2ba43f21c733378b25b47ca17cc3a6534ef61f
Rob Stevenson <robert.stevenson@shopify.com>
2022-01-11 15:11:23 +0000

    Allow FoodItems without unit
    This is preferable to needing to put e.g. 'whole' for vegetables
    which don't have a measure unit.

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,5 +2,4 @@ class FoodItem < ApplicationRecord
   alias_attribute :food_item_name, :name
   alias_attribute :food_item_unit, :unit
   validates :name, presence: true
-  validates :unit, presence: true
 end
