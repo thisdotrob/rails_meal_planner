ac3d39ea1eb8fe58b8308751093dcdbed96650eb
Rob Stevenson <robert.stevenson@shopify.com>
2022-02-21 21:44:59 +0000

    Bootstrap GraphQL
    Installed gem and ran `rails generate graphql:install`

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_121026) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_12_121026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "food_items", force: :cascade do |t|
     t.string "name"
     t.string "unit"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "carbs"
     t.boolean "deleted", default: false
   end
@@ -27,8 +26,8 @@ ActiveRecord::Schema.define(version: 2022_01_12_121026) do
   create_table "ingredients", force: :cascade do |t|
     t.bigint "recipe_id", null: false
     t.decimal "quantity_per_serving"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "food_item_id", null: false
     t.index ["food_item_id"], name: "index_ingredients_on_food_item_id"
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
@@ -37,26 +36,26 @@ ActiveRecord::Schema.define(version: 2022_01_12_121026) do
   create_table "meal_plan_days", force: :cascade do |t|
     t.date "date"
     t.bigint "meal_plan_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "note"
     t.index ["meal_plan_id"], name: "index_meal_plan_days_on_meal_plan_id"
   end
 
   create_table "meal_plans", force: :cascade do |t|
     t.daterange "date_range", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "meals", force: :cascade do |t|
     t.decimal "servings", null: false
     t.bigint "recipe_id", null: false
     t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "meal_plan_day_id", null: false
-    t.time "eat_at", null: false
+    t.time "eat_at"
     t.index ["meal_plan_day_id"], name: "index_meals_on_meal_plan_day_id"
     t.index ["recipe_id"], name: "index_meals_on_recipe_id"
   end
@@ -64,8 +63,8 @@ ActiveRecord::Schema.define(version: 2022_01_12_121026) do
   create_table "recipes", force: :cascade do |t|
     t.text "instructions"
     t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.decimal "fat"
     t.decimal "carbs"
     t.decimal "protein"
@@ -75,8 +74,8 @@ ActiveRecord::Schema.define(version: 2022_01_12_121026) do
   create_table "shopping_basket_items", force: :cascade do |t|
     t.bigint "meal_plan_id", null: false
     t.bigint "food_item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["food_item_id"], name: "index_shopping_basket_items_on_food_item_id"
     t.index ["meal_plan_id"], name: "index_shopping_basket_items_on_meal_plan_id"
   end
